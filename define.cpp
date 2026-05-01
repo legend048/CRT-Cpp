8def9d6e5eb0d97199be40aa46763dc54cf3e057
legend048 <pklegend048@gmail.com>
2026-05-01 09:41:00 +0530

    init

---
+++ define.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+#define sQ(x) (x) * (x)
+#define sq(x) x * x
+
+
+using namespace std;
+
+int main() {
+    cout<<sQ(2+1)-sq(2+1);
+    return 0;
+
+}
---
+++ l_b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int l, b;
+
+    cout << "Enter two integers: ";
+    cin >> l >> b;
+
+    cout<< l << " " << b << endl;
+    cout << "The larger number is: " << l << endl;
+    
+    return 0;
+}
